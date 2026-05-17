722fd5e204965a035e16811f13f7ff7daf330968
Salvador Ramirez <salva2006diaz@gmail.com>
2026-05-17 14:18:14 -0600

    Implement dynamic dashboard content loading

--- desktop-app/src/main/java/com/medsync/controllers/DashboardController.java
+++ desktop-app/src/main/java/com/medsync/controllers/DashboardController.java
@@ -9,6 +9,35 @@ package com.medsync.controllers;
  *
  * @author salva
  */
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
 
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    public void initialize() {
+        loadView("home.fxml");
+    }
+
+    public void loadView(String fxml) {
+
+        try {
+
+            AnchorPane view = FXMLLoader.load(getClass().getResource("/com/medsync/desktop/app/views/" + fxml));
+
+            contentPane.getChildren().setAll(view);
+
+        } catch (IOException e) {
+
+            System.out.println("Error cargando vista.");
+
+            e.printStackTrace();
+        }
+    }
 }
